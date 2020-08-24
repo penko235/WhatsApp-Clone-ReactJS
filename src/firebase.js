8b5ca0823927ea39b7455805b0b03e87b62dbe75
Penko Gadzhev <penko032@gmail.com>
2020-08-24 15:12:38 +0300

    Starting project whatsapp-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCD9G7u88kFis12ZuquBIsD9-Ey6GCwfo",
+  authDomain: "whatsapp-clone-27daa.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-27daa.firebaseio.com",
+  projectId: "whatsapp-clone-27daa",
+  storageBucket: "whatsapp-clone-27daa.appspot.com",
+  messagingSenderId: "999323101569",
+  appId: "1:999323101569:web:f221846a97360fcee1a080",
+  measurementId: "G-HDR7F3WJHR"
+};
