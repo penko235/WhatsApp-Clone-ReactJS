299f8712720caa781bc6116ba3c9e7e34d8c58b1
Penko Gadzhev <penko032@gmail.com>
2020-08-25 16:51:33 +0300

    Create Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello</h1>
-    </div>
-  );
+    <>
+      <div className="app">
+
+        <div className="app__body">
+          <Sidebar />
+          {/* {sidebar} */}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
